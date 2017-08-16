e228555b72be3fad81ff24c5ed6378a664ae68a5
Dan Davison <dandavison7@gmail.com>
2017-08-16 16:06:12 +0200

    Move parser into separate module

--- iterm2_dwim/__init__.py
+++ iterm2_dwim/__init__.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 from contextlib import contextmanager
-import os
-import re
 import subprocess
 import sys
 
 from iterm2_dwim.editors import emacs
 from iterm2_dwim.logger import log
+from iterm2_dwim.parser import get_path_and_line
 
 
 Editor = emacs.Editor
@@ -29,33 +28,6 @@ def notification_on_error():
         notify(msg)
         log(msg)
         exit(1)
-
-
-def get_path_and_line(path, text_after):
-    path = re.sub('\.pyc$', '.py', path)
-    match = (
-        # python stack trace, e.g.
-        # File "/path/to/somefile.py", line 336, in some_function
-        re.match(r'[^"]*", line (\d+).*', text_after) or
-        # ipdb stack trace
-        # > /path/to/somefile.py(336)some_function()
-        # Fails for
-        # /home/dan/nfs-share/website/counsyl/product/data_entry/tests/__init__.py(1005)assertKey()
-        re.match(r'[^(]*\((\d+)\).*', text_after) or
-        # counsyl/product/api/utils/fake.py:18:1:
-        re.match(r'[^:]*:(\d+):\d+:[^:]*', text_after)
-    )
-    if not match:
-        line = 1
-        log('%s (no line number)' % path)
-        log(path)
-        log(text_after)
-    else:
-        (line,) = match.groups()
-        line = int(line)
-        log('%s:%d' % (path, line))
-
-    return path, line
 
 
 def main():
---
+++ iterm2_dwim/parser.py
@@ -0,0 +1,30 @@
+import re
+
+from iterm2_dwim.logger import log
+
+
+def get_path_and_line(path, text_after):
+    path = re.sub('\.pyc$', '.py', path)
+    match = (
+        # python stack trace, e.g.
+        # File "/path/to/somefile.py", line 336, in some_function
+        re.match(r'[^"]*", line (\d+).*', text_after) or
+        # ipdb stack trace
+        # > /path/to/somefile.py(336)some_function()
+        # Fails for
+        # /home/dan/nfs-share/website/counsyl/product/data_entry/tests/__init__.py(1005)assertKey()
+        re.match(r'[^(]*\((\d+)\).*', text_after) or
+        # counsyl/product/api/utils/fake.py:18:1:
+        re.match(r'[^:]*:(\d+):\d+:[^:]*', text_after)
+    )
+    if not match:
+        line = 1
+        log('%s (no line number)' % path)
+        log(path)
+        log(text_after)
+    else:
+        (line,) = match.groups()
+        line = int(line)
+        log('%s:%d' % (path, line))
+
+    return path, line
