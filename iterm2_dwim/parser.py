37a00b1dde3ec3cc45f35a7610161909ccedff8a
Dan Davison <dandavison7@gmail.com>
2017-08-16 19:27:59 +0200

    Hack: handle relative paths

--- iterm2_dwim/parser.py
+++ iterm2_dwim/parser.py
@@ -4,11 +4,27 @@ import re
 from iterm2_dwim.logger import log
 
 
+# For relative paths to be handled, add something like the following to shell
+# prompt function to inform iterm2-dwim of the current directory:
+# echo $PWD > /tmp/cwd
+CWD_FILE = '/tmp/cwd'
+
+
 def get_path_and_line(path, text_after):
     if not path.startswith('/') and text_after.startswith('/'):
         # Hack: relative path followed by directory
         # SmartSelection path regex passing \0 \d
-        return os.path.join(text_after, path), 1
+        try:
+            with open(CWD_FILE) as fp:
+                cwd = fp.read().strip()
+        except IOError:
+            raise ParseError(
+                'Got path: %s\n'
+                'Interpreting as relative path, but current working '
+                'directory unknown.'
+            )
+        else:
+            return os.path.join(cwd, path), 1
 
     path = re.sub('\.pyc$', '.py', path)
     match = (
