df2f62c404137600e7d1b8e68120bb0e6da25129
Dan Davison <dandavison7@gmail.com>
2017-08-18 18:08:18 +0200

    Log parse function names as they are attempted

--- iterm2_dwim/parsers/__init__.py
+++ iterm2_dwim/parsers/__init__.py
@@ -7,6 +7,7 @@ from iterm2_dwim.parsers.parsers import ParseError
 
 def get_path_and_line(path_text, text_after):
     for parse_fn in PARSERS:
+        log(parse_fn.__name__)
         try:
             path, line = parse_fn(path_text, text_after)
         except ParseError as ex:
