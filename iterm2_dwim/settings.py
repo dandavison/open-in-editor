3998f450f3454eb9173a61f633220534c37fba56
Dan Davison <dandavison7@gmail.com>
2017-08-18 12:43:36 +0200

    Tweak editor settings text

--- iterm2_dwim/settings.py
+++ iterm2_dwim/settings.py
@@ -1,10 +1,10 @@
 # To select your editor, set one of the following to the absolute path to the
 # relevant executable file on your system.
 
-# For example, on some systems this would be
+# For example, if you have installed Emacs from homebrew, this should probably be
 # emacsclient = '/usr/local/bin/emacsclient'
 emacsclient = None
 
-# For example, on some systems this would be
+# For example, if you've installed Sublime from the .dmg, this should probably be
 # sublime = '/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'
 sublime = None
