afb0684e11ce462f5f4f3798f656119615d54619
danielkrausz <dnielkrausz@gmail.com>
2020-06-06 16:53:37 +0200

    Add VScode as an editor

---
+++ iterm2_dwim/editors/vscode.py
@@ -0,0 +1,11 @@
+import subprocess
+
+from iterm2_dwim.editors.base import BaseEditor
+from iterm2_dwim.logger import log
+
+
+class VSCode(BaseEditor):
+    def visit_file(self, path, line):
+        cmd = [self.executable, "-g", "%s:%s" % (path, line)]
+        log(" ".join(cmd))
+        subprocess.check_call(cmd)
