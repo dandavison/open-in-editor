7bd0c573d2d7f2ed95e1c5f9bb7c0d28e9b27b1f
Daniel Davison <ddavison@twitter.com>
2021-12-01 18:43:01 -0500

    Update hash digest again

--- casks/open-in-editor.rb
+++ casks/open-in-editor.rb
@@ -1,6 +1,6 @@
 cask "open-in-editor" do
   version "1.0"
-  sha256 "96ab091ea5f37d9b075ba61b11201464805eaf641af14482a295c664f1166965"
+  sha256 "68554df16c05a222f9fd95c8ce0f51a07e0a714db31dc43bf06e5addf591b15d"
 
   github_user = "dandavison"
   url "https://github.com/#{github_user}/#{token}/archive/master.zip"
